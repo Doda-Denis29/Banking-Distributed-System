abb36357f88be46c45480649636eaf3f5d61fe45
antiradzlmao@gmail.com <antiradzlmao@gmail.com>
2023-11-26 17:10:46 +0200

    Added icons and working on frontPage

---
+++ src/FrontEnd/GUI.java
@@ -0,0 +1,9 @@
+package FrontEnd;
+
+import javax.swing.*;
+
+public interface GUI {
+    public void createBasic(JFrame frame, float sizeX, float sizeY);
+    public void endBasic(JFrame frame);
+    public void createBasic(JFrame frame, float sizeX, float sizeY, ImageIcon icon);
+}
